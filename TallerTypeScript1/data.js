f7cbf26b72e1e7eaccd1b2309dae918fa1cad105
acardenasl <94406795+acardenasl@users.noreply.github.com>
2024-04-14 15:42:23 -0500

    Add files via upload

---
+++ TallerTypeScript1/data.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.series = void 0;
+var serie_1 = require("./serie");
+exports.series = [
+    new serie_1.Serie(1, "Breaking Bad", "AMC", 5),
+    new serie_1.Serie(2, "Orange Is the New Black", "Netflix", 6),
+    new serie_1.Serie(3, "Game of Thrones", "HBO", 7),
+    new serie_1.Serie(4, "The Big Bang Theory", "CBS", 12),
+    new serie_1.Serie(5, "Sherlock", "BBC", 4),
+    new serie_1.Serie(6, "A Very English Scandal", "BBC", 2),
+];
